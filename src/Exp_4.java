9c202e59ad68ada9628563c47ff7856a6453071e
Varad Chaudhari <varad.jnnwf22@sinhgad.edu>
2025-11-03 12:57:16 +0530

    final DAA

---
+++ src/Exp_4.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Exp_4 {
+
+    static int knapsack(int[] values, int[] weights, int capacity) {
+        int n = values.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        // Build table dp[][] in bottom-up manner
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (weights[i - 1] <= w)
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+
+        // Backtracking to find selected items
+        int res = dp[n][capacity];
+        int w = capacity;
+        System.out.println("Selected Items:");
+        for (int i = n; i > 0 && res > 0; i--) {
+            if (res != dp[i - 1][w]) {
+                System.out.println("Item with weight " + weights[i - 1] + " and value " + values[i - 1]);
+                res -= values[i - 1];
+                w -= weights[i - 1];
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int capacity = 50;
+
+        int maxValue = knapsack(values, weights, capacity);
+        System.out.println("Maximum value achievable: " + maxValue);
+    }
+}
---
+++ src/NQueensFirstQueen.java
@@ -0,0 +1,67 @@
+public class NQueensFirstQueen {
+
+    // Function to check if a queen can be placed at board[row][col]
+    static boolean isSafe(int[][] board, int row, int col, int n) {
+        // Check column above
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 1)
+                return false;
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
+    // Backtracking function to solve N-Queens
+    static boolean solveNQueensUtil(int[][] board, int row, int n) {
+        if (row == n)  // All queens placed
+            return true;
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 1;  // Place queen
+                if (solveNQueensUtil(board, row + 1, n))
+                    return true;       // Solution found
+                board[row][col] = 0;  // Backtrack
+            }
+        }
+        return false;  // No valid placement in this row
+    }
+
+    static void solveNQueensWithFirstQueen(int n, int firstRow, int firstCol) {
+        int[][] board = new int[n][n];
+        board[firstRow][firstCol] = 1;  // Place first queen
+
+        if (solveNQueensUtil(board, firstRow + 1, n)) {
+            System.out.println("Solution for " + n + "x" + n + " board with first queen at ("
+                    + firstRow + ", " + firstCol + "):\n");
+            printBoard(board, n);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    static void printBoard(int[][] board, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(board[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        int firstQueenRow = 0;
+        int firstQueenCol = 1;
+        System.out.println("For 8x8 Board with 8 Queens:\n");
+        solveNQueensWithFirstQueen(n, firstQueenRow, firstQueenCol);
+    }
+}
